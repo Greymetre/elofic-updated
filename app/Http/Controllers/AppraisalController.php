2536c4f5989bf298ecdb7371bdf7719024694666
Vaibhav <vaibhav.vishwakarma@lemosys.com>
2024-01-17 19:25:28 +0530

    worked on appraisal

--- app/Http/Controllers/AppraisalController.php
+++ app/Http/Controllers/AppraisalController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\AppraisalExport;
 use App\Models\Appraisal;
 use App\Models\salesWeightage;
 use App\Models\User;
 use Illuminate\Http\Request;
 use DataTables;
-use Auth;
 use Validator;
+use Excel;
+use Auth;
 use DB;
 
 class AppraisalController extends Controller
@@ -66,7 +68,7 @@ class AppraisalController extends Controller
         }
         if ($request->ajax()) {
             $data = Appraisal::select('year', 'user_id', DB::raw('GROUP_CONCAT(created_at) as dates'))->whereIn('user_id', $all_reporting_user_ids)->groupBy('year', 'user_id');
-            
+
             return Datatables::of($data)
                 ->addIndexColumn()
                 ->addColumn('user_name', function ($query) {
@@ -148,22 +150,23 @@ class AppraisalController extends Controller
         foreach ($sale_weightage_id as $key => $value) {
             Appraisal::updateOrCreate(
                 [
-                'weightage_id' => $value,
-                'user_id' => $executive_id,
-                'year' => $f_year,
+                    'weightage_id' => $value,
+                    'user_id' => $executive_id,
+                    'year' => $f_year,
+                    'rating_by' => $user_id,
                 ],
                 [
-                'weightage_id' => $value,
-                'user_id' => $executive_id,
-                'year' => $f_year,
-                'target' => $target[$key],
-                'achivment' => $achivment[$key],
-                'acual' => $acual[$key],
-                'rating' => $rating[$key],
-                'rating_by' => $user_id,
-                'appraisal_type' => $appraisal_type,
-                'appraisal_session' => $appraisal_session,
-                'remark' => $remark,
+                    'weightage_id' => $value,
+                    'user_id' => $executive_id,
+                    'year' => $f_year,
+                    'target' => $target[$key],
+                    'achivment' => $achivment[$key],
+                    'acual' => $acual[$key],
+                    'rating' => $rating[$key],
+                    'rating_by' => $user_id,
+                    'appraisal_type' => $appraisal_type,
+                    'appraisal_session' => $appraisal_session,
+                    'remark' => $remark,
                 ]
             );
         }
@@ -173,22 +176,189 @@ class AppraisalController extends Controller
 
     public function download(Request $request)
     {
-        dd($request->all());
+        $executive_id = $request->input('executive_id');
+        if ($executive_id && $executive_id != '' && $executive_id != null) {
+            $all_reporting_user_ids = array($executive_id);
+        } else {
+            $all_reporting_user_ids = getUsersReportingToAuth();
+        }
+        $data = [];
+        $appraisal = Appraisal::select(
+            DB::raw('GROUP_CONCAT(IFNULL(target, \'\')) as target'),
+            DB::raw('GROUP_CONCAT(weightage_id) as weightage_id'),
+            DB::raw('GROUP_CONCAT(year) as year'),
+            DB::raw('GROUP_CONCAT(user_id) as user_id'),
+            DB::raw('GROUP_CONCAT(achivment) as achivment'),
+            DB::raw('GROUP_CONCAT(rating) as rating'),
+            DB::raw('GROUP_CONCAT(rating_by) as rating_by'),
+        )->whereIn('user_id', $all_reporting_user_ids)->groupBy('year', 'user_id')->get();
+
+        $rportingByArray = Appraisal::whereIn('user_id', $all_reporting_user_ids)->groupBy('year', 'rating_by')->pluck('year', 'rating_by')->toArray();
+        
+        $userlistArray = Appraisal::whereIn('user_id', $all_reporting_user_ids)->groupBy('year', 'user_id')->pluck('user_id')->toArray();
+        
+        $all_grades =  array('Grtade By Self');
+
+
+        $first_head = [
+            'S.No',
+            'Branch',
+            'Department',
+            'Emp Code',
+            'Name',
+            'Designation',
+            'Date Of Joining',
+            'CTC',
+            'Last Yr Increments',
+            'Last Promotion',
+            'Sale Target',
+            'Sale Achivment',
+            'New Dealer Target',
+            'New Dealer Achivment',
+            'Saarthi Accu Point',
+            'Plumbers Meet Nos of Meets',
+            'Discipline',
+            '>60 Days OS',
+        ];
+        if (count($appraisal) > 0) {
+            foreach ($appraisal as $k => $val) {
+                $data[$k][0] = ++$k;
+                $data[$k][1] = $val->users->getbranch->branch_name;
+                $data[$k][2] = $val->users->getdepartment->division_name;
+                $data[$k][3] = $val->users->employee_codes;
+                $data[$k][4] = $val->users->name;
+                $data[$k][5] = $val->users->getdesignation->designation_name;
+                $data[$k][6] = $val->users->userinfo->date_of_joining ? date('d-M-y', strtotime($val->users->userinfo->date_of_joining)) : "";
+                $data[$k][7] = $val->users->userinfo->salary ?? "-";
+                $data[$k][8] = $val->users->userinfo->last_year_increments ?? "-";
+                $data[$k][9] = $val->users->userinfo->last_promotion ?? "-";
+                $all_tr = explode(',', $val->target);
+                $all_ach = explode(',', $val->achivment);
+
+                $data[$k][10] = $all_tr[count($all_tr) - 6] ?? "0";
+                $data[$k][11] = $all_ach[count($all_ach) - 6] ?? "0";
+                $data[$k][12] = $all_tr[count($all_tr) - 5] ?? "0";
+                $data[$k][13] = $all_ach[count($all_ach) - 5] ?? "0";
+                $data[$k][14] = $all_ach[count($all_ach) - 4] ?? "0";
+                $data[$k][15] = $all_ach[count($all_ach) - 3] ?? "0";
+                $data[$k][16] = $all_ach[count($all_ach) - 2] ?? "0";
+                $data[$k][17] = $all_ach[count($all_ach) - 1] ?? "0";
+
+                $all_reporting_by = explode(',', $val->rating_by);
+                $all_reporting_by = array_unique($all_reporting_by);
+                if (in_array($val->users->id, $all_reporting_by)) {
+                    $getRating = Appraisal::where('user_id', $val->users->id)->where('rating_by', $val->users->id)->get();
+                    $totalper = 0;
+                    if (count($getRating) > 0) {
+                        foreach ($getRating as $calcu) {
+                            $totalper += ($calcu->sales_weightage->weightage * $calcu->rating) / 10;
+                        }
+                        if ($totalper < 51) {
+                            $data[$k][18] = 'C';
+                        } else if ($totalper > 50 && $totalper < 61) {
+                            $data[$k][18] = 'B';
+                        } else if ($totalper > 60 && $totalper < 71) {
+                            $data[$k][18] = 'B+';
+                        } else if ($totalper > 70 && $totalper < 81) {
+                            $data[$k][18] = 'A';
+                        } else if ($totalper > 80) {
+                            $data[$k][18] = 'A+';
+                        }
+                    }
+                } else {
+                    $data[$k][18] = '-';
+                }
+            }
+        }
+        $rids = array();
+        foreach($userlistArray as $k=>$val){
+            $main_user = User::find($val);
+            ++$k;
+            $i = 0;
+            $remark = '-';
+            $Increment = 0;
+            foreach($rportingByArray as $k2=>$val2){
+                $rp_user = User::find($k2);
+                if($k2 != $val && $rp_user->roles[0]->id != $main_user->roles[0]->id){
+                    if(!in_array($rp_user->id, $rids)){
+                        array_push($all_grades, $rp_user->name.'('.$rp_user->roles[0]->name.')');
+                        array_push($rids, $rp_user->id);
+                    }
+                    $rats = Appraisal::where('year', $val2)->where('user_id', $val)->where('rating_by', $k2)->get();
+                    if(count($rats) > 0){
+                        $totalper = 0;
+                        foreach($rats as $fn){
+                            $totalper += ($fn->sales_weightage->weightage * $fn->rating) / 10;
+                            if ($totalper < 51) {
+                                $data[$k][19+$i] = 'C';
+                            } else if ($totalper > 50 && $totalper < 61) {
+                                $data[$k][19+$i] = 'B';
+                                if($rp_user->hasRole('Head office')){
+                                    $remark = $rats[0]->remark;
+                                    $Increment = '8%';
+                                }
+                            } else if ($totalper > 60 && $totalper < 71) {
+                                $data[$k][19+$i] = 'B+';
+                                if($rp_user->hasRole('Head office')){
+                                    $remark = $rats[0]->remark;
+                                    $Increment = '10%';
+                                }
+                            } else if ($totalper > 70 && $totalper < 81) {
+                                $data[$k][19+$i] = 'A';
+                                if($rp_user->hasRole('Head office')){
+                                    $remark = $rats[0]->remark;
+                                    $Increment = '12%';
+                                }
+                            } else if ($totalper > 80) {
+                                $data[$k][19+$i] = 'A+';
+                                if($rp_user->hasRole('Head office')){
+                                    $remark = $rats[0]->remark;
+                                    $Increment = '14%';
+                                }
+                            }
+                        }
+                    }else{
+                        $data[$k][19+$i] = "-";
+                    }
+                    $i++;
+                }
+            }
+            $data[$k][20+$i] = $Increment;
+            $data[$k][21+$i] = " ";
+            $data[$k][22+$i] = " ";
+            $data[$k][23+$i] = $remark;
+        }
+        $last_head = [
+            'Increment %',
+            'Final Amount',
+            'Promotion',
+            'Remark'
+        ];
+        $headings = array_merge(
+            $first_head,
+            $all_grades,
+            $last_head,
+        );
+        $export = new AppraisalExport($data, $headings, count($all_grades));
+
+        return Excel::download($export, 'AppraisalData.xlsx');
     }
 
-    public function getappraisal(Request $request){
-        if($request->appraisal_type == 'quarterly' || $request->appraisal_type == 'half_yearly'){
+    public function getappraisal(Request $request)
+    {
+        if ($request->appraisal_type == 'quarterly' || $request->appraisal_type == 'half_yearly') {
             $all_reporting_user_ids = getUsersReportingToAuth();
             $appraisal = Appraisal::with('sales_weightage')->where('user_id', $request->executive_id)->where('year', $request->f_year)->where('appraisal_type', $request->appraisal_type)->where('appraisal_session', $request->appraisal_session)->whereIn('rating_by', $all_reporting_user_ids)->get();
-            foreach($appraisal as $k=>$val){
-                $appraisal[$k]->rating_by_user->getdesignation = $val->rating_by_user->getdesignation;
-            }
-        }else{
+        } else {
             $all_reporting_user_ids = getUsersReportingToAuth();
             $appraisal = Appraisal::with('sales_weightage')->where('user_id', $request->executive_id)->where('year', $request->f_year)->where('appraisal_type', $request->appraisal_type)->whereIn('rating_by', $all_reporting_user_ids)->get();
-            foreach($appraisal as $k=>$val){
+        }
+        if(count($appraisal) > 0){
+            foreach ($appraisal as $k => $val) {
                 $appraisal[$k]->rating_by_user->getdesignation = $val->rating_by_user->getdesignation;
+                $appraisal[$k]->rating_by_user->roles = $val->rating_by_user->roles;
             }
+
         }
 
         return response()->json($appraisal);
