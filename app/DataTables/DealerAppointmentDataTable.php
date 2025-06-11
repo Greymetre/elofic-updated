690b433dcc3b51e4fec9a155a50d614fa4c2e91f
root <root@silver.fieldkonnect.io>
2025-06-11 13:43:48 +0000

    Added job queue and delaer appoint changes for delaer portal

--- app/DataTables/DealerAppointmentDataTable.php
+++ app/DataTables/DealerAppointmentDataTable.php
@@ -82,7 +82,9 @@ class DealerAppointmentDataTable extends DataTable
     {
         $user_ids = getUsersReportingToAuth();
         $data = $model->with('branch_details', 'district_details', 'city_details', 'appointment_kyc_detail');
-        if(!auth()->user()->hasRole('superadmin') && !auth()->user()->hasRole('Sub_Admin') && !auth()->user()->hasRole('Sub billing') && !auth()->user()->hasRole('Head office')){
+        if(auth()->user()->hasRole('Customer Dealer')){
+            $data->where('parent_id' , auth()->user()->customerid);
+        }elseif(!auth()->user()->hasRole('superadmin') && !auth()->user()->hasRole('Sub_Admin') && !auth()->user()->hasRole('Sub billing') && !auth()->user()->hasRole('Head office')){
             $data->whereIn('created_by' , $user_ids);
         }
 
