82c8b046d3970878d9d3d22fa7fbef9ec98dc2ce
mac <mac@macs-MacBook-Pro.local>
2026-08-13 18:01:42 +0530

    Changes in report sub menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2376,7 +2376,7 @@
                                  @endif -->
                         @if(auth()->user()->can('reports_sale'))
                         <li
-                            class="nav-link-btn add_icon {{ request()->is('reports/attendancereport') || request()->is('reports/reports_sale*') || request()->is('reports/fos_rating*') || request()->is('reports/primary_sales*') || request()->is('reports/secondary_sales*') || request()->is('reports/product_analysis_qty*') || request()->is('reports/product_analysis_branch*') || request()->is('reports/product_analysis_value*') || request()->is('reports/group_wise_analysis*') || request()->is('reports/asm_rating*') || request()->is('reports/ch_rating*') ? 'active' : '' }}">
+                            class="nav-link-btn add_icon {{ request()->is('reports/attendancereport') || request()->is('reports/attendancereportSummary') || request()->is('reports/reports_sale*') || request()->is('reports/fos_rating*') || request()->is('reports/primary_sales*') || request()->is('reports/secondary_sales*') || request()->is('reports/product_analysis_qty*') || request()->is('reports/product_analysis_branch*') || request()->is('reports/product_analysis_value*') || request()->is('reports/group_wise_analysis*') || request()->is('reports/asm_rating*') || request()->is('reports/ch_rating*') ? 'active' : '' }}">
                             <a class="hoveradd" data-toggle="collapse" href="#salesReportsMenu"
                                 aria-expanded="false">
                                 <i class="material-icons icon">point_of_sale</i>
@@ -2393,6 +2393,16 @@
                                             <i class="material-icons icon">report</i>
                                             <span>Attendance Detail Report</span>
                                             <div class="d-none mobile_hide">Attendance Detail Report</div>
+                                        </a>
+                                    </li>
+                                    @endif
+                                    @if(auth()->user()->can('attendance_summary_report'))
+                                    <li
+                                        class="nav-link-btn {{ request()->is('reports/attendancereportSummary') ? 'active' : '' }}">
+                                        <a class="hoveradd2" href="{{ url('reports/attendancereportSummary') }}">
+                                            <i class="material-icons icon">summarize</i>
+                                            <span>Attendance Summary Report</span>
+                                            <div class="d-none mobile_hide">Attendance Summary Report</div>
                                         </a>
                                     </li>
                                     @endif
