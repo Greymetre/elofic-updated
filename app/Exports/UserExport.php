798fa548946dd2c6a2fee6770dea13c35f800e4f
mac <mac@macs-MacBook-Pro.local>
2026-08-13 17:21:59 +0530

    Changes in user report export

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -69,7 +69,7 @@ class UserExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMa
     public function headings(): array
     {
         if (Auth::user()->id == 1) {
-            return ['ID', 'Employees Code', 'User Name', 'Designation', 'Role', 'Zone Name', 'Location', 'Department', 'Division', 'Reporting To', 'Mobile', 'Email',
+            return ['ID', 'Employees Code', 'User Name', 'Designation', 'Role', 'Zone Name', 'Base Location', 'Department', 'Division', 'Reporting To', 'Mobile', 'Email',
             //  'Gender',
               'Date Of Joining', 'Date Of Birth',
             //    'Age', 'CTC Annual', 'Gross Salary Monthly', 'CTC Per Month', 'Last Year Increments', 'Last Year Increment Percent', 'Last Year Increments Value', 'Last Promotion', 'Marital Status', 'Father Name', 'Father Date Of Birth', 'Mother Name', 'Mother  DOB', 'Marriage Anniversary', 'Spouse name', 'Spouse Date Of Birth', 'Children-1', 'Children-1 DOB', 'Children-2', 'Children-2 DOB', 'Children-3', 'Children-3 DOB', 'Children-4', 'Children-4 DOB', 'Children-5', 'Children-5 DOB', 'PAN Number', 'Adhar Number', 'Emergency Number', 'Current Address', 'Permanent Address', 'Biometric Code', 'Account Number', 'Bank Name', 'IFSC Code', 'PF Number', 'UN Number', 'ESI Number', 'Probation Period',
@@ -87,7 +87,7 @@ class UserExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMa
                     //    'Leave Balance'
                        ];
         } else {
-            return ['ID', 'Employees Code', 'User Name', 'Designation', 'Role', 'Zone Name', 'Location', 'Department', 'Division', 'Reporting To', 'Mobile', 'Email', 
+            return ['ID', 'Employees Code', 'User Name', 'Designation', 'Role', 'Zone Name', 'Base Location', 'Department', 'Division', 'Reporting To', 'Mobile', 'Email',
             // 'Gender', 
             'Date Of Joining', 'Date Of Birth', 
             // 'Age', 'Last Promotion', 'Marital Status', 'Father Name', 'Father Date Of Birth', 'Mother Name', 'Mother  DOB', 'Marriage Anniversary', 'Spouse name', 'Spouse Date Of Birth', 'Children-1', 'Children-1 DOB', 'Children-2', 'Children-2 DOB', 'Children-3', 'Children-3 DOB', 'Children-4', 'Children-4 DOB', 'Children-5', 'Children-5 DOB', 'PAN Number', 'Adhar Number', 'Emergency Number', 'Current Address', 'Permanent Address', 'Biometric Code', 'Account Number', 'Bank Name', 'IFSC Code', 'PF Number', 'UN Number', 'ESI Number', 'Probation Period',
@@ -230,7 +230,7 @@ class UserExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMa
                 $data['division_id'] ?? '-',
                 $data['department_id'] ?? '-',
                 // $data['warehouse_id'] ?? '-',
-                ($data['show_attandance_report'] == 1 ? 'Yes' : 'No'),
+                ($data['show_attandance_report'] == 1 ? 1 : 0),
                 // $data['userinfo'] ? $data['userinfo']['order_mails'] : '',
                 // implode(',', $mail_types_name),
                 // $data['userinfo'] ? $data['userinfo']['order_mails_type'] : '',
@@ -325,7 +325,7 @@ class UserExport implements FromCollection, WithHeadings, ShouldAutoSize, WithMa
                 $data['division_id'] ?? '-',
                 $data['department_id'] ?? '-',
                 // $data['warehouse_id'] ?? '-',
-                ($data['show_attandance_report'] == 1 ? 'Yes' : 'No'),
+                ($data['show_attandance_report'] == 1 ? 1 : 0),
                 // $data['userinfo'] ? $data['userinfo']['order_mails'] : '',
                 // implode(',', $mail_types_name),
                 // $data['userinfo'] ? $data['userinfo']['order_mails_type'] : '',
