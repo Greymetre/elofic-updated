67d771b991e776954f429c425859603464a5f152
Abhishek <developer11@greymetre.io>
2026-09-14 18:35:17 +0530

    work on  export

--- resources/views/reports/user_monthly_analysis.blade.php
+++ resources/views/reports/user_monthly_analysis.blade.php
@@ -1,8 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="content">
-    <div class="container-fluid">
+<x-app-layout>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
@@ -85,6 +81,4 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+</x-app-layout>
