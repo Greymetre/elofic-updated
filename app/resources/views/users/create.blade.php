476e366908996fd7f2f7d64d015efb8764e131a3
Abhishek <developer11@greymetre.io>
2026-09-16 15:47:52 +0530

    work on attendance

--- app/resources/views/users/create.blade.php
+++ app/resources/views/users/create.blade.php
@@ -571,10 +571,7 @@
                                             </div>
                                             @endif
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="input_section">
+
                                         <label class="col-form-label">Tour Plan</label>
                                         <div class="form-group has-default bmd-form-group">
                                             <input type="radio" name="show_tour_plan" value="1"
