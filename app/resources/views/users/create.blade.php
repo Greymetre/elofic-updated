5d9ed6a2126c9e8c84ba015c1b854b32779dabf7
Abhishek <developer11@greymetre.io>
2026-09-16 14:11:16 +0530

    work on attendance

--- app/resources/views/users/create.blade.php
+++ app/resources/views/users/create.blade.php
@@ -573,6 +573,22 @@
                                         </div>
                                     </div>
                                 </div>
+                                <div class="col-md-6">
+                                    <div class="input_section">
+                                        <label class="col-form-label">Tour Plan Mandatory</label>
+                                        <div class="form-group has-default bmd-form-group">
+                                            <input type="hidden" name="tour_plan_mandatory" value="0">
+                                            <input type="checkbox" name="tour_plan_mandatory" value="1"
+                                                {{ old('tour_plan_mandatory', $user->tour_plan_mandatory ?? 0) == 1 ? 'checked' : '' }}>
+                                            <span class="yes_no">Mandatory</span>
+                                            @if ($errors->has('tour_plan_mandatory'))
+                                            <div class="error">
+                                                <p class="text-danger">{{ $errors->first('tour_plan_mandatory') }}</p>
+                                            </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
 
 
                             </div>
